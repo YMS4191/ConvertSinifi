44bda5eb17884e5e2d46e1e036b574ec4e965b2f
muratvuranok <muratvuranok@hotmail.com>
2019-10-23 11:00:08 +0300

    Add project files.

---
+++ ConvertSinifi/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ConvertSinifi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(12, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(317, 73);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 98);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ ConvertSinifi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConvertSinifi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string sayi = "123";
+            byte    s1  = Convert.ToByte(sayi); 
+            sbyte   s2  = Convert.ToSByte(sayi);
+            short   s3  = Convert.ToInt16(sayi);
+            ushort  s4  = Convert.ToUInt16(sayi);
+            int     s5  = Convert.ToInt32(sayi);
+            uint    s6  = Convert.ToUInt32(sayi);
+            long    s7  = Convert.ToInt64(sayi);
+            ulong   s8  = Convert.ToUInt64(sayi);
+            float   s9  = Convert.ToSingle(sayi);
+            double  s10 = Convert.ToDouble(sayi);
+            decimal s11 = Convert.ToDecimal(sayi);
+             
+            string gelenSayi1 = Convert.ToString(s11);
+            string gelenSayi2 = s11.ToString();
+        }
+    }
+}
